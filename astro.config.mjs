65a68567292befc4122529c116baaf39bc424117
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-29 13:10:23 +0100

    typst math attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
 import { myRemarkPlugin } from './testRemarkPlugin.mjs'
 
 // https://astro.build/config
@@ -6,6 +7,6 @@ export default defineConfig({
 	site: 'https://example.com',
 	integrations: [],
 	markdown: {
-		remarkPlugins: [myRemarkPlugin]
+		remarkPlugins: [remarkMath, myRemarkPlugin]
 	}
 });
