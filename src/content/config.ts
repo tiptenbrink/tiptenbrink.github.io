fd87b88110f9b33344e4b5038ab421f3d5f6a860
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-11-11 14:45:41 +0100

    update, add draft feature

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
+		draft: z.boolean().optional()
 	}),
 });
 
